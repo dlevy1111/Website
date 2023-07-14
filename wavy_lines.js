e6833e99dbf968df1ea8ad96b0c336233d856eea
David Levy <77945203+dlevy1111@users.noreply.github.com>
2023-07-13 22:20:06 -0400

    Update wavy_lines.js

--- wavy_lines.js
+++ wavy_lines.js
@@ -1,18 +1,45 @@
 var lines = [];
-const width = window.outerWidth;
-const height = window.outerHeight;
+var w;
+var h;
 
+var cnv = document.getElementById("defaultCanvas0");
+
+class Wavy_line {
+    constructor() {
+        // this.line_color;
+        this.variation = (Math.random() + 2) * 50;
+        this.offset = float(Math.random() * 2 * 3.14);
+    }
+
+    display() {
+        stroke(255);
+        var ystart = h / 2;
+        for (var x = 0; x < w; x++) {
+            var x_val = x;
+            var y_val = ystart + 50000.0 * this.variation * cos(x / this.variation + this.offset) / (pow(x - w / 2, 2) + 50000.0);
+
+            point(x_val, y_val);
+        }
+    }
+}
 
 function setup() {
-    console.log("width", width);
-    console.log("height", height);
 
-    createCanvas(width, height);
-    background(0, 0, 0);
+    w = windowWidth;
+    h = windowHeight;
+
+    console.log("width", w);
+    console.log("height", h);
+
+    cnv = createCanvas(w, h);
+    cnv.parent("p5canvas");
+    cnv.position(0, 0);
+
+    background(7, 7, 45);
 
     var i = 0;
     while (i < 10) {
-        lines.push(new Wavy_line())
+        lines.push(new Wavy_line());
         i++;
     }
 
@@ -23,41 +50,22 @@ function setup() {
 }
 
 
-
 function draw() {
 
+    w = windowWidth;
+    h = windowHeight;
 
-}
+    console.log("width", w);
+    console.log("height", h);
 
-class Wavy_line {
+    cnv = createCanvas(w, h);
+    cnv.parent("p5canvas");
+    cnv.position(0, 0);
 
+    background(7, 7, 45);
 
-    constructor() {
-        // this.line_color;
-        this.variation = (Math.random() + 2) * 50;
-        this.offset = float(Math.random() * 1000);
+    for (var i = 0; i < lines.length; i++) {
+        l = lines[i];
+        l.display();
     }
-
-    display() {
-
-        stroke(generate_pastel());
-        var ystart = width / 2;
-        for (var x = 0; x < width; x++) {
-            var x_val = x;
-            var y_val = ystart + 50000.0 * this.variation * cos(x / this.variation + this.offset) / (pow(x - width / 2, 2) + 50000.0);
-
-            point(x_val, y_val);
-        }
-
-    }
-}
-
-function generate_pastel() {
-    var r = random(150, 256);
-    var g = random(150, 256);
-    var b = random(150, 256);
-
-    var pastel = (r, g, b);
-
-    return pastel;
 }
